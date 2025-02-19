c2c82818272c784d83bc4a9d3166ffbe8efb9bf3
ml <ml20061023@outlook.com>
2025-02-19 17:53:24 +0800

    Initial commit: Neovim configuration

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -1,8 +1,12 @@
 return {
     "mistricky/codesnap.nvim",
     build = "make build_generator",
-    -- opts = {
-    -- },
+    cmd = "CodeSnap",
+    keys = {
+        -- 正确映射 Visual 模式需要特殊处理选区范围 --
+        { "<leader>cs", ":<C-u>'<,'>CodeSnapSave<CR>", mode = "v", desc = "保存选中代码快照" }, -- 增加范围处理
+        { "<leader>ch", ":<C-u>'<,'>CodeSnapSaveHighlight<cr>", mode = "v", desc = "高亮代码快照" }
+    },
     config = function()
         require("codesnap").setup {
             save_path = "/mnt/c/Users/ml200/Pictures/CodeSnaps/",
@@ -21,8 +25,14 @@ return {
             watermark = "",
             bg_color = "#535c68",
             bg_padding = 0,
-
             prefix = "📸 ",
+             -- 关键修复配置 新增以下设置 --
+            has_highlight = true,         -- 启用高亮识别
+            highlight_group = "IncSearch",-- 指定有效的高亮组
+            highlight_options = {         -- 添加高亮渲染参数
+            delay = 100,
+            timeout = 1000
+        },
             -- prompt = "Enter a name for the snapshot: ",
             -- file_path = "~/Desktop/",
             -- file_name = nil,
@@ -31,9 +41,7 @@ return {
             -- highlight_file = nil,
             -- highlight_format = "png",
             -- highlight_options = {},
-            -- silent = false,
-            -- callback = nil,
-            
+            -- silent = false,       
         }
     end,
 }
