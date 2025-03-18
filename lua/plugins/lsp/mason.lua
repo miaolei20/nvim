6540906bdfcc926e174d80de24faa8e2bc34c212
ml200 <ml20061023@outlook.com>
2025-03-18 21:25:19 +0800

    imporve

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,26 +1,30 @@
 return {
   {
     "williamboman/mason.nvim",
-    event = "VeryLazy", -- 延迟加载
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
+    event = "VeryLazy", -- 延遲加載
     config = function()
       -- Mason 核心配置
       require("mason").setup({
         ui = {
-          border = "rounded", -- 圆角边框
+          border = "rounded", -- 圓角邊框
           icons = {
-            package_installed = "󰄬", -- 安装成功图标
-            package_pending = "󰁔",   -- 安装中图标
-            package_uninstalled = "󰚌", -- 未安装图标
+            package_installed = "󰄬", -- 安裝成功圖標
+            package_pending = "󰁔",   -- 安裝中圖標
+            package_uninstalled = "󰚌", -- 未安裝圖標
           },
         },
-        max_concurrent_installers = 4, -- 并发安装数
-        log_level = vim.log.levels.INFO, -- 日志级别
+        max_concurrent_installers = 4, -- 並發安裝數
+        log_level = vim.log.levels.INFO, -- 日誌級別
       })
-
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    after = "mason.nvim", -- 確保在 mason.nvim 之後加載
+    dependencies = {
+      "williamboman/mason.nvim", -- 明確指定依賴關係
+    },
+    config = function()
       -- Mason LSP 配置
       require("mason-lspconfig").setup({
         ensure_installed = {
@@ -31,10 +35,18 @@ return {
           "jsonls",          -- JSON LSP
           "yamlls",          -- YAML LSP
         },
-        automatic_installation = true, -- 自动安装 LSP
+        automatic_installation = true, -- 自動安裝 LSP
       })
-
-      -- Mason 工具安装器配置
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    after = "mason-lspconfig.nvim", -- 確保在 mason-lspconfig.nvim 之後加載
+    dependencies = {
+      "williamboman/mason.nvim", -- 明確指定依賴關係
+    },
+    config = function()
+      -- Mason 工具安裝器配置
       require("mason-tool-installer").setup({
         ensure_installed = {
           "stylua",          -- Lua 格式化工具
@@ -43,8 +55,8 @@ return {
           "shfmt",           -- Shell 格式化工具
           "prettier",        -- JSON/YAML 格式化工具
         },
-        auto_update = true, -- 自动更新工具
-        run_on_start = true, -- 启动时检查工具
+        auto_update = true, -- 自動更新工具
+        run_on_start = true, -- 啟動時檢查工具
       })
     end,
   },
