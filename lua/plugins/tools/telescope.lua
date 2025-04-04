d40753fc09b98117d3993fb78ec6801ba05155b8
miaolei20 <ml20061023@outlook.com>
2025-04-04 15:58:14 +0800

    Initial commit

--- lua/plugins/tools/telescope.lua
+++ lua/plugins/tools/telescope.lua
@@ -13,46 +13,92 @@ return {
     "benfowler/telescope-luasnip.nvim",
   },
   keys = {
-    { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Files" },
-    { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Grep" },
+    { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<CR>",  desc = "Live Grep" },
     { "<leader>fd", "<cmd>Telescope diagnostics<CR>", desc = "Diagnostics" },
-    { "<leader>fb", "<cmd>Telescope file_browser<CR>", desc = "Browser" },
-    { "<leader>fu", "<cmd>Telescope undo<CR>", desc = "Undo" },
-    { "<leader>fr", "<cmd>Telescope frecency<CR>", desc = "Recent" },
-    { "<leader>fl", "<cmd>Telescope lazy<CR>", desc = "Plugins" },
-    { "<leader>fs", "<cmd>Telescope luasnip<CR>", desc = "Snippets" },
+    { "<leader>fb", "<cmd>Telescope file_browser<CR>", desc = "File Browser" },
+    { "<leader>fu", "<cmd>Telescope undo<CR>",        desc = "Undo History" },
+    { "<leader>fr", "<cmd>Telescope frecency<CR>",   desc = "Frecency" },
+    { "<leader>fl", "<cmd>Telescope lazy<CR>",       desc = "Lazy Plugins" },
+    { "<leader>fs", "<cmd>Telescope luasnip<CR>",    desc = "Snippets" },
   },
   opts = {
     defaults = {
-      layout_config = { width = 0.95, height = 0.85 },
+      layout_strategy = "horizontal",
+      layout_config = {
+        horizontal = { 
+          width = 0.95,
+          height = 0.85,
+          preview_width = 0.55,
+        },
+        vertical = { width = 0.85, height = 0.85 },
+      },
       path_display = { "truncate" },
-      file_ignore_patterns = { ".git/", "node_modules/" },
+      file_ignore_patterns = {
+        "^.git/",
+        "^node_modules/",
+        "^.venv/",
+        "^.cache/",
+        "%.lock",
+      },
       mappings = {
         i = {
           ["<C-j>"] = "move_selection_next",
           ["<C-k>"] = "move_selection_previous",
           ["<Esc>"] = "close",
-          ["<CR>"] = "select_default",
+          ["<CR>"]  = "select_default",
         },
       },
     },
     pickers = {
-      find_files = { hidden = true },
+      find_files = {
+        hidden = true,
+        no_ignore = false,
+        find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
+      },
       diagnostics = { theme = "ivy" },
       luasnip = { theme = "dropdown" },
     },
     extensions = {
-      fzf = { fuzzy = true },
-      file_browser = { hijack_netrw = true },
-      ["ui-select"] = { layout_strategy = "dropdown", previewer = false },
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+      },
+      file_browser = {
+        hijack_netrw = true,
+        hidden = true,
+        grouped = true,
+      },
+      ["ui-select"] = require("telescope.themes").get_dropdown({
+        previewer = false,
+        layout_config = {
+          width = 0.8,
+          height = 0.5,
+        }
+      }),
     },
   },
   config = function(_, opts)
     local telescope = require("telescope")
     telescope.setup(opts)
-    local extensions = { "fzf", "file_browser", "undo", "frecency", "ui-select", "lazy", "luasnip" }
+
+    -- Safe load extensions
+    local extensions = {
+      "fzf",
+      "file_browser",
+      "undo",
+      "frecency",
+      "ui-select",
+      "lazy",
+      "luasnip"
+    }
+
     for _, ext in ipairs(extensions) do
-      pcall(telescope.load_extension, ext)
+      local ok, _ = pcall(telescope.load_extension, ext)
+      if not ok then
+        vim.notify("Failed to load telescope extension: " .. ext, vim.log.levels.WARN)
+      end
     end
   end,
 }
